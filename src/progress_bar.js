426af2978c6a6fd15014f0bf451b6324a122583d
chus-chus <jemanac1998@gmail.com>
2024-02-29 20:39:06 +0100

    bounded scrollbar

--- src/progress_bar.js
+++ src/progress_bar.js
@@ -16,22 +16,26 @@ document.addEventListener('DOMContentLoaded', function() {
         marker.textContent = section.getAttribute('id'); // Use the section's ID as text
         // Calculate position percentage (top offset / total content height)
         const positionPercent = (section.offsetTop / contentHeight) * 100;
-        marker.style.top = positionPercent + '%'; // Set the top position as a percentage
+        marker.style.top = Math.max(0, (positionPercent - 15)) + '%'; // Set the top position as a percentage
         progressBarContainer.appendChild(marker); // Append marker to the progress container
     });
 
     function updateProgressBar() {
         const scrollDistance = window.scrollY;
-        const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
-        const scrolledPercentage = (scrollDistance / maxScroll) * 100;
+
+        const maxScroll = document.documentElement.scrollHeight - window.innerHeight;   
+
+    // Calculate the progress bar height as a percentage of the total scrollable height
+        let scrolledPercentage = Math.min(0.888, (scrollDistance / maxScroll)) * 100;
+
         progressBar.style.height = scrolledPercentage + '%';
 
         // Check proximity to each section to adjust the opacity of the progress bar
         let nearestSectionDistance = Number.MAX_VALUE; // Start with the maximum distance
 
         sections.forEach(section => {
-            const sectionTop = section.offsetTop - window.innerHeight * 0.01; // Start checking 10% above the section
-            const sectionBottom = section.offsetTop + section.offsetHeight + window.innerHeight * 0.01; // Continue checking 10% below the section
+            const sectionTop = section.offsetTop - window.innerHeight * 0.1; // Start checking 10% above the section
+            const sectionBottom = section.offsetTop + section.offsetHeight + window.innerHeight * 0.1; // Continue checking 10% below the section
 
             // Determine the distance to the nearest section
             if (scrollDistance > sectionTop && scrollDistance < sectionBottom) {
@@ -43,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Adjust the opacity based on the nearest section distance
         // The closer to the section, the lower the opacity
-        const opacity = 1 - Math.min(1, nearestSectionDistance / (window.innerHeight * 0.99));
+        const opacity = Math.min(1, nearestSectionDistance / (window.innerHeight * 0.99) + 0.05);
         progressBar.style.opacity = opacity;
     }
 
