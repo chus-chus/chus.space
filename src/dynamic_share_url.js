b66861b44288b7c25c260a82dee02bdecd64b6a1
chus-chus <jemanac1998@gmail.com>
2024-03-01 19:57:01 +0100

    removed comments

--- src/dynamic_share_url.js
+++ src/dynamic_share_url.js
@@ -3,30 +3,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     var twitterBtn = document.querySelector('a[aria-label="Twitter"]');
     if (twitterBtn) {
-        var baseUrl = window.location.href.split('#')[0]; // Remove the hash part
+        var baseUrl = window.location.href.split('#')[0]; // Remove hash
         twitterBtn.href = 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(baseUrl);
     }
 });
 
 
-function copyToClipboard() { // Removed the url parameter since it's always the current URL being copied
-    var baseUrl = window.location.href.split('#')[0]; // Remove the hash part
-    // Create a temporary text area to hold the URL
+function copyToClipboard() {
+    var baseUrl = window.location.href.split('#')[0]; // Remove hash
+    // temporary text area to hold the URL
     var tempInput = document.createElement('textarea');
     // Prevent styling from affecting layout
     tempInput.style.position = 'absolute';
     tempInput.style.left = '-9999px';
-    tempInput.value = baseUrl; // Use the modified URL
+    tempInput.value = baseUrl;
     document.body.appendChild(tempInput);
     tempInput.select();
     document.execCommand('copy');
     document.body.removeChild(tempInput);
 
-    // Show the notification
+    // Show notification
     var notification = document.getElementById('copy-notification');
     notification.style.display = 'block';
     
-    // Hide the notification after 2 seconds
+    // Hide notification after 2s
     setTimeout(function() {
         notification.style.display = 'none';
     }, 2000);
