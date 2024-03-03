e760c64766c1688ae3214a3b73f7a8b47e91a03f
chus-chus <jemanac1998@gmail.com>
2024-03-03 18:37:02 +0100

    More examples

--- src/viz/test.js
+++ src/viz/test.js
@@ -1,5 +1,8 @@
 import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";
+                        
+const plot2 = Plot.rectY({length: 10000}, Plot.binX({y: "count"}, {x: Math.random})).plot();
+const plot3 = Plot.rectY({length: 10000}, Plot.binX({y: "count"}, {x: Math.random})).plot();
 
-const plot = Plot.rectY({length: 10000}, Plot.binX({y: "count"}, {x: Math.random})).plot();
-const div = document.querySelector("#myplot");
-div.append(plot);
+const divplot2 = document.querySelector("#plot2");
+divplot2.append(plot2);
+divplot2.append(plot3);
